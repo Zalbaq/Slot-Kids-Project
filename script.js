38376300af8db16b96ffc4e1003b94c83e80471e
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-05 13:48:18 +0700

    Add randoming items function

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const SLOT_ITEMS = {
-    item1: "/icon/cherries.png",
-    item2: "/icon/diamond.png",
-    item3: "/icon/lemon.png",
-    item4: "/icon/plum.png",
-    item5: "/icon/star.png"
-}
-const BUTTON_SPIN = document.querySelector('#button-spin');
-const ITEM1 = document.getElementById('item1').querySelectorAll('img');
+  item1: {
+    src: "/icon/cherries.png",
+    diff: 5,
+  },
+  item2: {
+    src: "/icon/diamond.png",
+    diff: 4,
+  },
+  item3: {
+    src: "/icon/lemon.png.png",
+    diff: 3,
+  },
+  item4: {
+    src: "/icon/plum.png",
+    diff: 2,
+  },
+  item5: {
+    src: "/icon/star.png",
+    diff: 1,
+  },
+};
 
+const BUTTON_SPIN = document.querySelector("#button-spin");
+const ITEM1 = document.getElementById("item1").querySelectorAll("img");
+const ITEM2 = document.getElementById("item2").querySelectorAll("img");
+const ITEM3 = document.getElementById("item3").querySelectorAll("img");
+const ITEM4 = document.getElementById("item4").querySelectorAll("img");
 
-BUTTON_SPIN.addEventListener('click', ()=>{
-    randomItem(ITEM1);
-})
-let randomItem = (target)=>{
-    target.forEach(element => {
-        const RANDOM_ITEM = Math.floor(Math.random()*5) + 1;
-        element.src = SLOT_ITEMS[`item${RANDOM_ITEM}`]
-    });
-} 
+let getRandom = () => {
+  const RANDOM = Math.floor(Math.random() * 5) + 1;
+  return RANDOM;
+};
+let checkRandomItem = () => {
+    if (getRandom) {
+        
+    }
+};
