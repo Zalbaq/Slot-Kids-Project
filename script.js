540fb94eac114185320e2f53f8e3c3f306d4f8cf
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-05 17:27:28 +0700

    Create function for button spin

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const SLOT_ITEMS = {
   item1: {
     src: "/icon/cherries.png",
-    diff: 5,
+    diff: 13,
   },
   item2: {
     src: "/icon/diamond.png",
-    diff: 4,
+    diff: 8,
   },
   item3: {
-    src: "/icon/lemon.png.png",
-    diff: 3,
+    src: "/icon/lemon.png",
+    diff: 5,
   },
   item4: {
     src: "/icon/plum.png",
-    diff: 2,
+    diff: 3,
   },
   item5: {
     src: "/icon/star.png",
@@ -22,17 +22,36 @@ const SLOT_ITEMS = {
 };
 
 const BUTTON_SPIN = document.querySelector("#button-spin");
-const ITEM1 = document.getElementById("item1").querySelectorAll("img");
-const ITEM2 = document.getElementById("item2").querySelectorAll("img");
-const ITEM3 = document.getElementById("item3").querySelectorAll("img");
-const ITEM4 = document.getElementById("item4").querySelectorAll("img");
+const SLOT1 = document.getElementById("item1").querySelectorAll("img");
+const SLOT2 = document.getElementById("item2").querySelectorAll("img");
+const SLOT3 = document.getElementById("item3").querySelectorAll("img");
+const SLOT4 = document.getElementById("item4").querySelectorAll("img");
+const SLOT5 = document.getElementById("item5").querySelectorAll("img");
 
-let getRandom = () => {
-  const RANDOM = Math.floor(Math.random() * 5) + 1;
-  return RANDOM;
+let getSlotPowers = () => {
+  return Math.floor(Math.random() * 5) + 1;
 };
-let checkRandomItem = () => {
-    if (getRandom) {
-        
+
+let getItemSLot = (SLOT)=>{
+  SLOT.forEach(element => {
+    let powerItem = getSlotPowers();
+    if (powerItem >= SLOT_ITEMS.item1.diff) {
+      element.src = SLOT_ITEMS.item1.src;
+    } else if (powerItem >= SLOT_ITEMS.item2.diff) {
+      element.src = SLOT_ITEMS.item2.src;
+    }else if (powerItem >= SLOT_ITEMS.item3.diff) {
+      element.src = SLOT_ITEMS.item3.src;
+    }else if (powerItem >= SLOT_ITEMS.item4.diff) {
+      element.src = SLOT_ITEMS.item4.src;
+    } else{
+      element.src = SLOT_ITEMS.item5.src;
     }
-};
+  });
+}
+BUTTON_SPIN.addEventListener('click', ()=>{
+  getItemSLot(SLOT1);
+  getItemSLot(SLOT2);
+  getItemSLot(SLOT3);
+  getItemSLot(SLOT4);
+  getItemSLot(SLOT5);
+})
