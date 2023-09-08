64c51ff61fca2b734d71a6500d89f5d428742ec4
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-08 23:42:45 +0700

    Create roll function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const ICON_WIDTH = 250,
   ICON_HEIGHT = 300,
-  TIME_PER_ICON = 100;
-  INDEXES_ICON = [0,0,0];
+  NUM_ICONS = 6,
+  TIME_PER_ITEM = 50,
+  INDEXES = [0, 0, 0, 0, 0];
 
-const ITEMS = document.querySelectorAll('.item');
+const BTN_SPIN = document.querySelector("#button-spin");
 
-const getRoll = ()=>{
-
-}
+const roll = (reel, offset = 0) => {
+  const random =
+    (offset + 2) * NUM_ICONS + Math.round(Math.random() * NUM_ICONS);
+  const style = getComputedStyle(reel),
+    backgroundPositionY = parseFloat(style["background-position-y"]);
+  reel.style.transition = `background-position-y ${
+    5 + random * TIME_PER_ITEM
+  }ms`;
+  reel.style.backgroundPositionY = `${
+    backgroundPositionY + random * ICON_HEIGHT
+  }px`;
+};
