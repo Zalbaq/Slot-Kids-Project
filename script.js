5fe7689f83f5534070594f75cebbb06da0c696fc
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-13 19:58:43 +0700

    Adding slot logics for reel

--- script.js
+++ script.js
@@ -16,18 +16,42 @@ const ICON_WIDTH = 100,
     100000, 200000, 500000, 1000000, 2000000, 5000000, 15000000, 20000000,
   ];
 let INDEX_BET = 0;
-
 let cash = 9000000000;
+
 const CREDIT_COMPONENT = document.querySelector(".credit");
-CREDIT_COMPONENT.textContent = cash;
 const BTN_SPIN = document.querySelector("#button-spin");
 const PLUS_BET = document.querySelector("#plus-button"),
   MIN_BET = document.querySelector("#min-button"),
   BET = document.querySelector("#bet");
 
+let gacor = (random) => {
+  if (random >= 1 && random < 6) {
+    return 0;
+  } else if (random >= 6 && random < 13) {
+    return 1;
+  } else if (random >= 13 && random < 21) {
+    return 2;
+  } else if (random >= 21 && random < 30) {
+    return 3;
+  } else if (random >= 30 && random < 41) {
+    return 4;
+  } else if (random >= 41 && random < 53) {
+    return 5;
+  } else if (random >= 53 && random < 66) {
+    return 6;
+  } else if (random >= 66 && random < 80) {
+    return 7;
+  } else {
+    return 8;
+  }
+};
+
+CREDIT_COMPONENT.textContent = cash;
 const roll = (reel, offset = 0) => {
-  const delta =
-    (offset + 2) * NUM_ICONS + Math.round(Math.random() * NUM_ICONS);
+  let random = Math.round(Math.random() * 94);
+  // const delta =
+  //   (offset + 2) * NUM_ICONS + Math.round(Math.random() * NUM_ICONS);
+  const delta = (offset + 2) * NUM_ICONS + gacor(random);
   const style = getComputedStyle(reel),
     backgroundPositionY = parseFloat(style["background-position-y"]),
     targetBackgroundPositionY = backgroundPositionY + delta * ICON_HEIGHT,
@@ -55,19 +79,18 @@ const rollAll = () => {
     deltas.forEach(
       (delta, i) => (INDEXES[i] = (INDEXES[i] + delta) % NUM_ICONS)
     );
-    // INDEXES.map((index) => console.log(ICON_MAP[index]));
     INDEXES.map((index) => console.log(ICON_MAP[index]));
-    if (
-      (INDEXES[0] == INDEXES[1] || INDEXES[0] == INDEXES[1]) == INDEXES[2] ||
-      ((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3] ||
-      (((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3]) ==
-        INDEXES[4] ||
-      ((((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3]) ==
-        INDEXES[4]) ==
-        INDEXES[5]
-    ) {
-      console.log("winnnnn");
-    }
+    // if (
+    //   (INDEXES[0] == INDEXES[1] || INDEXES[0] == INDEXES[1]) == INDEXES[2] ||
+    //   ((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3] ||
+    //   (((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3]) ==
+    //     INDEXES[4] ||
+    //   ((((INDEXES[0] == INDEXES[1]) == INDEXES[2]) == INDEXES[3]) ==
+    //     INDEXES[4]) ==
+    //     INDEXES[5]
+    // ) {
+    //   console.log("winnnnn");
+    // }
   });
 };
 
@@ -99,3 +122,10 @@ MIN_BET.addEventListener("click", () => {
     BET.textContent = BETS[INDEX_BET];
   }
 });
+
+// window.addEventListener("resize", () => {
+//   let width = window.innerWidth;
+//   let height = window.innerHeight;
+//   document.querySelector("#width-device").textContent = width;
+//   document.querySelector("#height-device").textContent = height;
+// });
